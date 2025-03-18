18c2790273edcb998b1e424976677c9636db2b2e
Giorgio Agnello <93651368+GiorgioAgn@users.noreply.github.com>
2025-03-18 18:20:37 +0100

    Fix 2_ Skills mobile images

--- src/components/skillsSection/AllSkillsSM.jsx
+++ src/components/skillsSection/AllSkillsSM.jsx
@@ -1,17 +1,6 @@
 import { FaHtml5 } from "react-icons/fa";
 import { FaCss3Alt } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io";
-import { SiTypescript } from "react-icons/si";
-import { FaReact } from "react-icons/fa";
-import { SiRedux } from "react-icons/si";
-import { SiNextdotjs } from "react-icons/si";
-import { RiTailwindCssFill } from "react-icons/ri";
-import { motion } from "framer-motion";
-import { fadeIn } from "../../framerMotion/variants";
-import SingleSkill from "./SingleSkill";
-import { FaHtml5 } from "react-icons/fa";
-import { FaCss3Alt } from "react-icons/fa";
-import { IoLogoJavascript } from "react-icons/io";
 import { FaReact } from "react-icons/fa";
 import { SiNextdotjs } from "react-icons/si";
 import { RiTailwindCssFill } from "react-icons/ri";
@@ -34,16 +23,16 @@ const skills = [
     icon: IoLogoJavascript,
   },
   {
-    skill: "SQL",
-    icon: PiFileSqlDuotone,
-  },
-  {
     skill: "ReactJS",
     icon: FaReact,
   },
   {
-    skill: "Java",
-    icon: RiJavaFill,
+      skill: "Java",
+      icon: RiJavaFill,
+    },
+  {
+      skill: "SQL",
+      icon: PiFileSqlDuotone,
   },
   {
     skill: "NextJS",

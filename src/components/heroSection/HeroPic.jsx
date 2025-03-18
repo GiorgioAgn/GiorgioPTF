05fe17fd4a4f8216e31bf969c426a5a7a4d367fa
Giorgio Agnello <93651368+GiorgioAgn@users.noreply.github.com>
2025-03-18 18:02:12 +0100

    Prova /images/...

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,8 +12,8 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="/HexaPic.png"
-        alt="Abdelrahman Qassem"
+        src="/images/HexaPic.png"
+        alt="Giorgio Agnello"
         className="max-h-[450px] w-auto"
       />
 

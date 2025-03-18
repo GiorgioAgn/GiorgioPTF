a89aab0c0d7d9e0d97b3dce406a03629910cb922
Giorgio Agnello <93651368+GiorgioAgn@users.noreply.github.com>
2025-03-18 17:54:37 +0100

    Fix - percorso assoluto

--- src/components/experienceSection/ExperienceTopMiddle.jsx
+++ src/components/experienceSection/ExperienceTopMiddle.jsx
@@ -1,7 +1,7 @@
 const ExperienceTopMiddle = () => {
   return (
     <div className="lg:w-[20%] md:w-[50%] sm:w-[60%]">
-      <img src="../../public/images/experience-image.png" alt="my experience" />
+      <img src="/experience-image.png" alt="my experience" />
     </div>
   );
 };
